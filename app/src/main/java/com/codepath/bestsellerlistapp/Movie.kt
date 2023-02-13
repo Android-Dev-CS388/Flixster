c41a5207f203d183b2b40ebfb83ca39f7228c0cc
tejpatel <tp29@njit.edu>
2023-02-13 01:45:16 -0500

    Edit Files from Books to Movie

--- app/src/main/java/com/codepath/bestsellerlistapp/Movie.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/Movie.kt
@@ -8,23 +8,17 @@ import com.google.gson.annotations.SerializedName
  * SerializedName tags MUST match the JSON response for the
  * object to correctly parse with the gson library.
  */
-class BestSellerBook {
-    @SerializedName("rank")
-    var rank = 0
-
+class Movie {
     @JvmField
     @SerializedName("title")
     var title: String? = null
 
-    @JvmField
-    @SerializedName("author")
-    var author: String? = null
-
     //TODO bookImageUrl
-
+    @SerializedName("poster_path")
+    var movieImageUrl: String? = null
 
     //TODO description
+    @SerializedName("overview")
+    var description: String? = null
 
-
-    //TODO-STRETCH-GOALS amazonUrl
 }
